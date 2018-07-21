aa61bcd739c8d42b2de795cc62818d00b7aa6a28
Gurpreet Juttla <gurpreet.juttla@gmail.com>
2018-07-21 22:00:56 +0200

    Use soft tabs

--- cpp/fib_TMP.cpp
+++ cpp/fib_TMP.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 template <int n>
 struct fib {
-	static const int val = fib<n - 1>::val + fib<n - 2>::val;
+    static const int val = fib<n - 1>::val + fib<n - 2>::val;
 };
 
 template<>
 struct fib<0> {
-	static const int val = 0;
+    static const int val = 0;
 };
 
 template<>
 struct fib<1> {
-	static const int val = 1;
+    static const int val = 1;
 };
 
 int main(int argc, char* argv[]) {
-	cout << fib<10>::val << endl;
-	return 0;
+    cout << fib<10>::val << endl;
+    return 0;
 }
