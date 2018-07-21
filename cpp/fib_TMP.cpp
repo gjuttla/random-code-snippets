223f5f10d8a4f91da98aa8d339f8b1a0a12ac874
Gurpreet Juttla <gurpreet.juttla@gmail.com>
2018-07-21 21:58:54 +0200

    Add template meta prog. version of Fibonacci

---
+++ cpp/fib_TMP.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+template <int n>
+struct fib {
+	static const int val = fib<n - 1>::val + fib<n - 2>::val;
+};
+
+template<>
+struct fib<0> {
+	static const int val = 0;
+};
+
+template<>
+struct fib<1> {
+	static const int val = 1;
+};
+
+int main(int argc, char* argv[]) {
+	cout << fib<10>::val << endl;
+	return 0;
+}
